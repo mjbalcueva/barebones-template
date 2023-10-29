f6f1df50f64f930bc7e1c3a7d7f7d79ef47d18f8
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2023-10-29 19:32:56 +0800

    fix: move font loader outside function

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,14 @@ import { Inter } from "next/font/google"
 
 import "./globals.css"
 
+const inter = Inter({ subsets: ["latin"] })
+
 export const metadata: Metadata = {
 	title: "Template Title",
 	description: "Template Description",
 }
 
 const AppLayout = ({ children }: React.PropsWithChildren) => {
-	const inter = Inter({ subsets: ["latin"] })
-
 	return (
 		<html lang="en">
 			<body className={inter.className}>{children}</body>
